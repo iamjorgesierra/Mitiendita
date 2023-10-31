d513974428e3058cb024353b49daf35155d57ea3
iamjorgesierra <90064326+iamjorgesierra@users.noreply.github.com>
2023-10-30 21:17:51 -0500

    postgres

--- src/database/sequelize.config.js
+++ src/database/sequelize.config.js
@@ -1,23 +1,13 @@
-//const dotenv = require("dotenv");
-//dotenv.config();
+const { Sequelize } = require('sequelize');
 
-//const { Sequelize } = require("sequelize");
-
-//const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
-  //host: process.env.DBHOST,
-  //dialect: "mysql",
-//});
-
-//module.exports = { sequelize };
-
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres", 
+const sequelize = new Sequelize({
+  dialect: 'postgres', // Selecciona el dialecto de la base de datos (PostgreSQL en este caso).
+  host: 'dpg-cl05ilbjdq6s73a1ikhg-a', // Reemplaza con el host de tu base de datos Render.
+  port: 5432, // Puerto por defecto de PostgreSQL.
+  username: 'mitiendita_user', // Reemplaza con tu nombre de usuario de la base de datos.
+  password: 'vJGNYraNPqIy8zcmVYjstVlkVUvDI9hR', // Reemplaza con tu contraseña.
+  database: 'mitiendita', // Reemplaza con el nombre de tu base de datos.
+  logging: false, // Puedes habilitar esto para ver consultas SQL en la consola si es necesario.
 });
 
-module.exports = { sequelize };
+module.exports = sequelize;
