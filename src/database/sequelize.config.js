3f60ce6db46bbb5bb42e59452d6caaae0c00df36
iamjorgesierra <90064326+iamjorgesierra@users.noreply.github.com>
2023-10-30 22:59:31 -0500

    postgres

--- src/database/sequelize.config.js
+++ src/database/sequelize.config.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DBURL)
+const sequelize = new Sequelize(process.env.DBURL, {
+  dialect: 'postgres', // Agrega el dialecto de PostgreSQL aquí.
+});
 
 module.exports = { sequelize };
